91b10d5da233d887360cf7f465b0b86854598143
syhw <gabriel.synnaeve@gmail.com>
2012-03-09 16:18:58 +0100

    fixes for totest.py + features upgrades/units

--- analyzis/totext.py
+++ analyzis/totext.py
@@ -18,32 +18,53 @@ header = True
 body = False
 names = {}
 races = {}
-time_built = {}
+time_had = {}
 buildings = {'T' : ['Expansion', 'Comsat_Station', 'Nuclear_Silo', 'Supply_Depot', 'Refinery', 'Barracks', 'Academy', 'Factory', 'Starport', 'Control_Tower', 'Science_Facility', 'Covert_Ops', 'Physics_Lab', 'Machine_Shop', 'Engineering_Bay', 'Armory', 'Missile_Turret', 'Bunker'],
         'P' : ['Expansion', 'Robotics_Facility', 'Pylon', 'Assimilator', 'Observatory', 'Gateway', 'Photon_Cannon', 'Citadel_of_Adun', 'Cybernetics_Core', 'Templar_Archives', 'Forge', 'Stargate', 'Fleet_Beacon', 'Arbiter_Tribunal', 'Robotics_Support_Bay', 'Shield_Battery'], 
         'Z' : ['Overlord', 'Infested_Command_Center', 'Expansion', 'Lair', 'Hive', 'Nydus_Canal', 'Hydralisk_Den', 'Defiler_Mound', 'Greater_Spire', 'Queens_Nest', 'Evolution_Chamber', 'Ultralisk_Cavern', 'Spire', 'Spawning_Pool', 'Creep_Colony', 'Spore_Colony', 'Sunken_Colony', 'Extractor']}
+doubling = {'T' : ['Expansion', 'Refinery', 'Barracks', 'Factory', 'Armory'],
+        'P' : ['Expansion', 'Pylon', 'Assimilator', 'Gateway'],
+        'Z' : ['Expansion', 'Extractor']}
+tripling = {'T' : ['Expansion', 'Barracks', 'Factory'],
+        'P' : ['Expansion', 'Gateway'],
+        'Z' : ['Expansion']}
+quadrupling = {'T' : ['Expansion'],
+        'P' : ['Expansion', 'Gateway'],
+        'Z' : ['Expansion']}
+units = {'T' : ['SCV', 'Dropship', 'Marine', 'Ghost', 'Vulture', 'Goliath', 'Siege Tank Tank Mode', 'Wraith', 'Science Vessel', 'Battlecruiser', 'Siege Tank Siege Mode', 'Firebat', 'Medic', 'Valkyrie' ], 
+        'P' : ['Probe', 'Shuttle', 'Observer', 'Dragoon', 'Zealot', 'Archon', 'Reaver', 'High Templar', 'Arbiter', 'Carrier', 'Scout', 'Dark Archon', 'Corsair', 'Dark Templar'], 
+        'Z' : ['Drone', 'Overlord', 'Zergling', 'Devourer', 'Guardian', 'Ultralisk', 'Queen', 'Hydralisk', 'Mutalisk', 'Scourge', 'Lurker', 'Defiler']}
+rchupgs = {'T' : ['Spider_Mines', ],
+        'P' : ['Zealot_Speed', 'Psionic_Storm', 'Dragoon_Range'],
+        'Z' : ['Zergling_Speed', 'Hydralisk Speed', 'Hydralisk Range']}
 
 def write(tb, fo):
     print tb
     for p, t in tb.iteritems():
         for e in buildings[races[p][0]]:
             b = races[p] + '_' + e
-            fo.write(b + ' ' + str(int(t.get(b, '0'))/24) + '; ')
-        for e in buildings[races[p][0]]:
+            fo.write(b + ' ' + str(t.get(b, 0)/24) + '; ')
+        for e in doubling[races[p][0]]:
             b = races[p] + '_' + e + '2'
-            fo.write(b + ' ' + str(int(t.get(b, '0'))/24) + '; ')
-        for e in buildings[races[p][0]]:
+            fo.write(b + ' ' + str(t.get(b, 0)/24) + '; ')
+        for e in tripling[races[p][0]]:
             b = races[p] + '_' + e + '3'
-            fo.write(b + ' ' + str(int(t.get(b, '0'))/24) + '; ')
-        for e in buildings[races[p][0]]:
+            fo.write(b + ' ' + str(t.get(b, 0)/24) + '; ')
+        for e in quadrupling[races[p][0]]:
             b = races[p] + '_' + e + '4'
-            fo.write(b + ' ' + str(int(t.get(b, '0'))/24) + '; ')
+            fo.write(b + ' ' + str(t.get(b, 0)/24) + '; ')
+        for e in units[races[p][0]]:
+            u = races[p] + '_' + e
+            fo.write(u + ' ' + str(t.get(u, 0)/24) + '; ')
+        for e in rchupgs[races[p][0]]:
+            r = races[p] + '_' + e
+            fo.write(r + ' ' + str(t.get(r, 0)/24) + '; ')
         fo.write('\n')
 
 for line in f:
     if not header and line[0] == '_':
-        write(time_built, o)
-        time_built = {}
+        write(time_had, o)
+        time_had = {}
         names = {}
         races = {}
         header = True
@@ -55,7 +76,7 @@ for line in f:
         if "Human" in line: # lol
             l = line.split(',')
             names[l[1]] = l[0][1:]
-            time_built[l[1]] = {}
+            time_had[l[1]] = {}
             if l[2] == 'P':
                 races[l[1]] = "Protoss"
             elif l[2] == 'T':
@@ -63,20 +84,34 @@ for line in f:
             elif l[2] == 'Z':
                 races[l[1]] = "Zerg"
     elif body:
-        if "Build" in line:
-            l = line.split(',')
+        l = line.split(',')
+        if "Build" in line or ("Train" in line and "Overlord" in line):
             bname = l[-1].rstrip('\r\n')
             if bname == 'Hatchery' or bname == 'Command Center' or bname == 'Nexus':
                 bname = 'Expansion'
             building = races[l[2]] + '_' + bname.replace(' ', '_')
-            if not building in time_built:
-                time_built[l[2]][building] = l[0] # in frames
-            elif not building+'2' in time_built:
-                time_built[building+'2'] = l[0]
-            elif not building+'3' in time_built:
-                time_built[building+'3'] = l[0]
-            elif not building+'4' in time_built:
-                time_built[building+'4'] = l[0]
-
-
+            if not building in time_had[l[2]]:
+                time_had[l[2]][building] = max(24, int(l[0])) # in frames
+            elif not building+'2' in time_had[l[2]]:
+                time_had[l[2]][building+'2'] = max(24, int(l[0]))
+            elif not building+'3' in time_had[l[2]]:
+                time_had[l[2]][building+'3'] = max(24, int(l[0]))
+            elif not building+'4' in time_had[l[2]]:
+                time_had[l[2]][building+'4'] = max(24, int(l[0]))
+        if "Train" in line:
+            uname = l[-1].rstrip('\r\n')
+            unit = races[l[2]] + '_' + uname.replace(' ', '_')
+            if not unit in time_had[l[2]]:
+                time_had[l[2]][unit] = max(24, int(l[0]))
+        if "Upgrade" in line or "Research" in line:
+            rname = l[-1].rstrip('\r\n')
+            if '(' in rname:
+                rname = rname.split('(')[1].rstrip(')')
+            rchupg = races[l[2]] + '_' + rname.replace(' ', '_')
+            if not rchupg in time_had[l[2]]:
+                time_had[l[2]][rchupg] = max(24, int(l[0]))
+            if not rchupg+'2' in time_had[l[2]]:
+                time_had[l[2]][rchupg] = max(24, int(l[0]))
+            if not rchupg+'3' in time_had[l[2]]:
+                time_had[l[2]][rchupg] = max(24, int(l[0]))
 
