ee8dbce3e81bdfb7cbfaac06db79192bad580337
syhw <gabriel.synnaeve@gmail.com>
2012-03-15 17:14:39 +0100

    updated totext.py

--- analyzis/totext.py
+++ analyzis/totext.py
@@ -10,9 +10,16 @@ import sys
 
 f = sys.stdin
 o = sys.stdout
+do_bots = False
+do_races = False
 if len(sys.argv) > 1:
     f = open(sys.argv[1], 'r')
     o = open(sys.argv[1][:-3]+'txt', 'w')
+    if "-bots" in sys.argv:
+        do_bots = True
+    if "-races" in sys.argv:
+        do_races = True
+
 
 header = True
 body = False
@@ -47,40 +54,54 @@ def write(tb, fo):
             b = races[p] + '_' + e
             twr = b + ' ' + str(t.get(b, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
         for e in doubling[races[p][0]]:
             b = races[p] + '_' + e + '2'
             twr = b + ' ' + str(t.get(b, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
         for e in tripling[races[p][0]]:
             b = races[p] + '_' + e + '3'
             twr = b + ' ' + str(t.get(b, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
         for e in quadrupling[races[p][0]]:
             b = races[p] + '_' + e + '4'
             twr = b + ' ' + str(t.get(b, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
         for e in units[races[p][0]]:
             u = races[p] + '_' + e
             twr = u + ' ' + str(t.get(u, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
         for e in rchupgs[races[p][0]]:
             r = races[p] + '_' + e
             twr = r + ' ' + str(t.get(r, -1)/24) + '; '
             fo.write(twr)
-            files_bots[names[p]].write(twr)
-            files_races[races[p]].write(twr)
-        files_bots[names[p]].write('\n')
-        files_races[races[p]].write('\n')
+            if do_bots:
+                files_bots[names[p]].write(twr)
+            if do_races:
+                files_races[races[p]].write(twr)
+        if do_bots:
+            files_bots[names[p]].write('\n')
+        if do_races:
+            files_races[races[p]].write('\n')
         fo.write('\n')
 
 for line in f:
@@ -91,59 +112,63 @@ for line in f:
         races = {}
         header = True
         body = False
+        o.write('\n') # separate games
     if header and line[0].isdigit():
         header = False
         body = True
     if header:
         if "Human" in line: # lol
             l = line.split(',')
-            names[l[1]] = l[0][1:]
-            if not l[0][1:] in files_bots:
+            names[l[-6]] = l[0][1:]
+            if do_bots and not l[0][1:] in files_bots:
                 files_bots[l[0][1:]] = open(l[0][1:]+".txt", 'w')
-            time_had[l[1]] = {}
-            if l[2] == 'P':
-                races[l[1]] = "Protoss"
-            elif l[2] == 'T':
-                races[l[1]] = "Terran"
-            elif l[2] == 'Z':
-                races[l[1]] = "Zerg"
-            if not races[l[1]] in files_races:
-                files_races[races[l[1]]] = open(races[l[1]]+'_'+l[2]+".txt", 'w')
+            time_had[l[-6]] = {}
+            if l[-5] == 'P':
+                races[l[-6]] = "Protoss"
+            elif l[-5] == 'T':
+                races[l[-6]] = "Terran"
+            elif l[-5] == 'Z':
+                races[l[-6]] = "Zerg"
+            if do_races and not races[l[-6]] in files_races:
+                files_races[races[l[-6]]] = open(races[l[-6]]+'_'+l[-5]+".txt", 'w')
     elif body:
         l = line.split(',')
         if "Build" in line or ("Train" in line and "Overlord" in line):
             bname = l[-1].rstrip('\r\n')
             if bname == 'Hatchery' or bname == 'Command Center' or bname == 'Nexus':
                 bname = 'Expansion'
-            building = races[l[2]] + '_' + bname.replace(' ', '_')
-            if not building in time_had[l[2]]:
-                time_had[l[2]][building] = max(24, int(l[0])) # in frames
-            elif not building+'2' in time_had[l[2]]:
-                time_had[l[2]][building+'2'] = max(24, int(l[0]))
-            elif not building+'3' in time_had[l[2]]:
-                time_had[l[2]][building+'3'] = max(24, int(l[0]))
-            elif not building+'4' in time_had[l[2]]:
-                time_had[l[2]][building+'4'] = max(24, int(l[0]))
+            plyr = l[l.index("Build") - 1]
+            building = races[plyr] + '_' + bname.replace(' ', '_')
+            if not building in time_had[plyr]:
+                time_had[plyr][building] = max(24, int(l[0])) # in frames
+            elif not building+'2' in time_had[plyr]:
+                time_had[plyr][building+'2'] = max(24, int(l[0]))
+            elif not building+'3' in time_had[plyr]:
+                time_had[plyr][building+'3'] = max(24, int(l[0]))
+            elif not building+'4' in time_had[plyr]:
+                time_had[plyr][building+'4'] = max(24, int(l[0]))
         if "Train" in line:
             uname = l[-1].rstrip('\r\n')
-            unit = races[l[2]] + '_' + uname.replace(' ', '_')
-            if not unit in time_had[l[2]]:
-                time_had[l[2]][unit] = max(24, int(l[0]))
+            unit = races[l[-5]] + '_' + uname.replace(' ', '_')
+            if not unit in time_had[l[-5]]:
+                time_had[l[-5]][unit] = max(24, int(l[0]))
         if "Upgrade" in line or "Research" in line:
             rname = l[-1].rstrip('\r\n')
             if '(' in rname:
                 rname = rname.split('(')[1].rstrip(')')
-            rchupg = races[l[2]] + '_' + rname.replace(' ', '_')
-            if not rchupg in time_had[l[2]]:
-                time_had[l[2]][rchupg] = max(24, int(l[0]))
-            if not rchupg+'2' in time_had[l[2]]:
-                time_had[l[2]][rchupg] = max(24, int(l[0]))
-            if not rchupg+'3' in time_had[l[2]]:
-                time_had[l[2]][rchupg] = max(24, int(l[0]))
+            rchupg = races[l[-5]] + '_' + rname.replace(' ', '_')
+            if not rchupg in time_had[l[-5]]:
+                time_had[l[-5]][rchupg] = max(24, int(l[0]))
+            if not rchupg+'2' in time_had[l[-5]]:
+                time_had[l[-5]][rchupg] = max(24, int(l[0]))
+            if not rchupg+'3' in time_had[l[-5]]:
+                time_had[l[-5]][rchupg] = max(24, int(l[0]))
 
-for p, f in files_bots.iteritems():
-    f.close()
-for p, f in files_races.iteritems():
-    f.close()
+if do_bots:
+    for p, f in files_bots.iteritems():
+        f.close()
+if do_races:
+    for p, f in files_races.iteritems():
+        f.close()
 o.close()
 
